f76c27d6248d28c7137ed0e4d5e40f9e4bd623e7
Carlos <cmanosalvas95@gmail.com>
2016-12-05 21:42:11 -0500

    practicas 2 y 3

---
+++ practica2.py
@@ -0,0 +1,28 @@
+print("--------------- TIPOS DE DATOS ---------------")
+
+#Tipos de datos
+# print(type(10))
+# print(type(5.5))
+# print(type(1+5j))
+
+print(type(10))
+print(type(5.5))
+print(type(1+5j))
+
+print(2 + 4)
+print(8**3)
+print((1+5j)+(1+8j))
+
+
+print("Esto s una cadena")
+print('Esto es una cadena con comillas simple')
+print("""Linea uno
+linea dos
+linea tres
+""")
+print(type("Esto s una cadena"))
+print("Cadena 1 "+"Cadena 2")
+print("Cadena 1 "*2)
+print(type(True))
+print(type(False))
+
---
+++ practica3.py
@@ -0,0 +1,35 @@
+a = 5
+print (a)
+print (type(a))
+a="cinco"
+print(a)
+print (type(a))
+
+palabra = "canción"
+
+print(palabra)
+
+a=10
+a+=9
+print(a)
+
+print(3&2)
+print(3<<1)
+
+
+
+nombre1="carlos"
+nombre2 ="Carlos"
+print(nombre1==nombre2)
+
+anio_nacimiento=1995
+print("Mayor de Edad")
+print((2016-anio_nacimiento)>=18)
+edad=60
+print("Adolescente??")
+print(12<= edad <=18)
+print("Edad no laborar")
+
+print(1<= edad <=18 or 80<=edad<=120)
+
+
