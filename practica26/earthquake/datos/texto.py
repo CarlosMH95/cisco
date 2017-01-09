ad953920b4021a4d842b80a30079b317e170fcba
carlos <cmanosalvas95@gmail.com>
2017-01-09 18:46:50 -0500

    Practica con modulos

---
+++ practica26/earthquake/datos/texto.py
@@ -0,0 +1,50 @@
+
+import requests
+url = "http://earthquake.usgs.gov/fdsnws/event/1/query"
+
+def validar_magnitud(func):
+    def envoltura(evento):
+        datos = func(evento)
+        if datos[10] == '' or float(datos[10]) <= 6:
+            return None
+        else:
+            return datos
+    return envoltura
+
+def preparar_url(format = "text", **kwargs):
+    global url
+    url += "?format=%s"%format
+    argumentos = ""
+    for k,v in kwargs.items():
+        argumentos += "&%s=%s" % (k,v)
+    url += argumentos
+
+def realizar_requerimiento():
+    response = requests.get(url)
+    return response.text
+
+def obtener_eventos(base_completa, q="ecuador"):
+    eventos = base_completa.split("\n")
+    resultado = []
+    for evento in eventos:
+        if evento.lower().find(q.lower()) >= 0:
+            resultado.append(evento)
+    return resultado
+
+@validar_magnitud
+def obtener_datos(evento):
+    return evento.split("|")
+
+def presentacion_datos(datos):
+    for dato in datos:
+        plantilla = """
+*********************************************
+magnitud = %s
+lugar = %s
+longitud = %s
+latitud = %s
+*********************************************
+""" % (dato[10], dato[12], dato[3], dato[2])
+        print(plantilla)
+
+#############################################################################
---
+++ practica26/main.py
@@ -0,0 +1,14 @@
+from practica26.earthquake.datos.texto import *
+##Deberia poner las fucniones para que no se cargue
+
+preparar_url(starttime="2016-04-15", endtime="2016-04-30",
+             orderby = "magnitude", limit = "20")
+base = realizar_requerimiento()
+eventos = obtener_eventos(base)
+data = []
+for evento in eventos:
+    lista = obtener_datos(evento)
+    if lista is not None:
+        data.append(lista)
+
+presentacion_datos(data)
